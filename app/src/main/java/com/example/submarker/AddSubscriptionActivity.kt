24c36c4d2f6452d0d1681e0de66cbf9946c438c2
Keith Lam <keithlam0110@gmail.com>
2023-04-09 02:58:10 +0800

    add category spinner

--- app/src/main/java/com/example/submarker/AddSubscriptionActivity.kt
+++ app/src/main/java/com/example/submarker/AddSubscriptionActivity.kt
@@ -2,6 +2,8 @@ package com.example.submarker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.example.submarker.databinding.ActivityAddSubscriptionBinding
 
 class AddSubscriptionActivity : AppCompatActivity() {
@@ -12,6 +14,17 @@ class AddSubscriptionActivity : AppCompatActivity() {
         setContentView(binding.root)
         setSupportActionBar(binding.backableToolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        // setup page spinner
+        val categories = resources.getStringArray(R.array.categories)
+        val categorySpinner = findViewById<Spinner>(R.id.spinner_category)
+        if (categorySpinner != null) {
+            val adapter = ArrayAdapter(
+                this,
+                R.layout.spinner_item, categories
+            )
+            categorySpinner.adapter = adapter
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
