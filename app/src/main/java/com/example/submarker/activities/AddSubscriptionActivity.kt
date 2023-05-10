bd4aa4291942c5b99da21497ddc74fed40943970
winkydes <keithlam0110@gmail.com>
2023-05-10 17:39:42 +0800

    add suggested subscription dialog and autofill function

--- app/src/main/java/com/example/submarker/activities/AddSubscriptionActivity.kt
+++ app/src/main/java/com/example/submarker/activities/AddSubscriptionActivity.kt
@@ -1,23 +1,31 @@
 package com.example.submarker.activities
 
+import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.view.Window
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.submarker.R
+import com.example.submarker.adapters.SuggestedSubscriptionAdapter
+import com.example.submarker.data.SuggestedSubscription
 import com.example.submarker.databinding.ActivityAddSubscriptionBinding
 import com.example.submarker.dialogFragment.DatePickerFragment
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import java.text.SimpleDateFormat
 import java.time.LocalDate
-import java.time.LocalTime
 
 class AddSubscriptionActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAddSubscriptionBinding
     private lateinit var tvPaymentDate: TextView
+    private var suggestedSubscriptionList: ArrayList<SuggestedSubscription> = ArrayList()
+    private lateinit var categorySpinner: Spinner
+    private lateinit var recyclerView: RecyclerView
     private val db = Firebase.firestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,75 +35,76 @@ class AddSubscriptionActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         // textview setup
-        tvPaymentDate = findViewById(R.id.tv_payment_date)
+        tvPaymentDate = binding.tvPaymentDate
         val date = LocalDate.now().dayOfMonth.toString()
         tvPaymentDate.text = date
 
         // setup page spinner
         val categories = resources.getStringArray(R.array.categories)
-        val categorySpinner = findViewById<Spinner>(R.id.spinner_category)
-        if (categorySpinner != null) {
-            val adapter = ArrayAdapter(
-                this,
-                R.layout.spinner_item, categories
-            )
-            categorySpinner.adapter = adapter
-        }
+        categorySpinner = binding.spinnerCategory
+        val categoryAdapter = ArrayAdapter(
+            this,
+            R.layout.spinner_item, categories
+        )
+        categorySpinner.adapter = categoryAdapter
         val period = resources.getStringArray(R.array.period)
-        val periodSpinner = findViewById<Spinner>(R.id.spinner_period)
-        if (periodSpinner != null) {
-            val adapter = ArrayAdapter(
-                this,
-                R.layout.spinner_item, period
-            )
-            periodSpinner.adapter = adapter
-            periodSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-                override fun onNothingSelected(p0: AdapterView<*>?) {
-
-                }
-                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                    if (parent?.selectedItemPosition == 0)
-                        tvPaymentDate.text = LocalDate.now().dayOfMonth.toString()
-                    else {
-                        val sdf = SimpleDateFormat("dd/MM")
-                        val dateString: String = sdf.format(System.currentTimeMillis())
-                        tvPaymentDate.text = dateString
-                    }
-                }
+        val periodSpinner = binding.spinnerPeriod
+        val periodAdapter = ArrayAdapter(
+            this,
+            R.layout.spinner_item, period
+        )
+        periodSpinner.adapter = periodAdapter
+        periodSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(p0: AdapterView<*>?) {
 
             }
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                if (parent?.selectedItemPosition == 0)
+                    tvPaymentDate.text = LocalDate.now().dayOfMonth.toString()
+                else {
+                    val sdf = SimpleDateFormat("dd/MM")
+                    val dateString: String = sdf.format(System.currentTimeMillis())
+                    tvPaymentDate.text = dateString
+                }
+            }
+
         }
 
         // setup onClickListener
-        val btnDatePicker = findViewById<Button>(R.id.btn_date_picker)
-        btnDatePicker.setOnClickListener {view ->
+        val btnDatePicker = binding.btnDatePicker
+        btnDatePicker.setOnClickListener {
             val datePicker = DatePickerFragment.newInstance(periodSpinner.selectedItem.toString())
             datePicker.show(supportFragmentManager, "datePicker")
         }
 
-        val btnSubmit = findViewById<Button>(R.id.btn_submit)
-        btnSubmit.setOnClickListener {view ->
-            if (findViewById<EditText>(R.id.et_name).text.toString().isEmpty() || findViewById<EditText>(R.id.et_price).text.toString().isEmpty()) {
+        val btnSubmit = binding.btnSubmit
+        btnSubmit.setOnClickListener {
+            if (binding.etName.text.toString().isEmpty() || binding.etPrice.text.toString().isEmpty()) {
                 Toast.makeText(this@AddSubscriptionActivity, "Some fields are still missing!", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
             val sharedPref = this.getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE) ?: null
             val userId = sharedPref?.getString("UUID", "") ?:""
-            var paymentDate = findViewById<TextView>(R.id.tv_payment_date).text.toString()
+            var paymentDate = binding.tvPaymentDate.text.toString()
             if (periodSpinner.selectedItem.toString() == "Month") {
                 paymentDate = paymentDate.substringBefore('/')
             }
             val subscription = hashMapOf<String, Any>(
                 "userID" to userId,
                 "category" to categorySpinner.selectedItem.toString(),
-                "name" to findViewById<EditText>(R.id.et_name).text.toString(),
-                "period" to findViewById<EditText>(R.id.et_period).text.toString(),
+                "name" to binding.etName.text.toString(),
+                "period" to binding.etPeriod.text.toString(),
                 "periodType" to periodSpinner.selectedItem.toString(),
                 "paymentDate" to paymentDate,
-                "price" to findViewById<EditText>(R.id.et_price).text.toString()
+                "price" to binding.etPrice.text.toString()
             )
             db.collection("subscriptions").add(subscription)
             onBackPressedDispatcher.onBackPressed()
+        }
+
+        val tvSearchButton = binding.btnSearchName
+        tvSearchButton.setOnClickListener {
+            showPossibleNameDialog()
         }
     }
 
@@ -106,5 +115,51 @@ class AddSubscriptionActivity : AppCompatActivity() {
 
     fun onDateSet(date:String) {
         tvPaymentDate.text = date
+    }
+
+    private fun showPossibleNameDialog() {
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(true)
+        dialog.setContentView(R.layout.dialog_possible_name)
+        recyclerView = dialog.findViewById(R.id.rv_possible_name)
+        loadNames()
+        recyclerView.adapter = SuggestedSubscriptionAdapter(suggestedSubscriptionList, ::changeInfo, dialog)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+//        val body = dialog.findViewById(R.id.body) as TextView
+//        body.text = title
+//        val yesBtn = dialog.findViewById(R.id.yesBtn) as Button
+//        val noBtn = dialog.findViewById(R.id.noBtn) as Button
+//        yesBtn.setOnClickListener {
+//            dialog.dismiss()
+//        }
+//        noBtn.setOnClickListener {
+//            dialog.dismiss()
+//        }
+        dialog.show()
+    }
+
+    private fun loadNames() {
+        db.collection("DefaultSubscription").whereEqualTo("category", categorySpinner.selectedItem.toString()).get().addOnSuccessListener { documents ->
+            try {
+                suggestedSubscriptionList.clear()
+                for (document in documents) {
+                    val sub: SuggestedSubscription = document.toObject(SuggestedSubscription::class.java)
+                    suggestedSubscriptionList.add(sub)
+                }
+                recyclerView.adapter?.run {
+                    notifyDataSetChanged()
+                }
+                Log.d("TAG", recyclerView.adapter?.itemCount.toString())
+            } catch (ex: Exception) {
+                ex.message?.let { Log.e("TAG", it) }
+            }
+        }
+    }
+
+    public fun changeInfo(name: String, price: String, period: String) {
+        binding.etName.setText(name)
+        binding.etPrice.setText(price)
     }
 }
---
+++ app/src/main/java/com/example/submarker/adapters/SuggestedSubscriptionAdapter.kt
@@ -0,0 +1,51 @@
+package com.example.submarker.adapters
+
+import android.app.Dialog
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.submarker.R
+import com.example.submarker.data.SuggestedSubscription
+import kotlin.reflect.KFunction3
+
+class SuggestedSubscriptionAdapter(private val dataSet: ArrayList<SuggestedSubscription>, private val callBack: KFunction3<String, String, String, Unit>, private val dialog: Dialog) :
+    RecyclerView.Adapter<SuggestedSubscriptionAdapter.ViewHolder>() {
+
+        class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+            private val tvSubName: TextView = itemView.findViewById(R.id.tv_subscription_name)
+            private val tvPrice: TextView = itemView.findViewById(R.id.tv_price)
+            private val tvPeriod: TextView = itemView.findViewById(R.id.tv_period)
+            fun bind(suggestedSubscription: SuggestedSubscription, callBack: (name: String, price: String, period: String) -> Unit, dialog: Dialog) {
+                tvSubName.text = suggestedSubscription.name
+                tvPrice.text = "$" + suggestedSubscription.price.toString().substringBefore('.')
+                tvPeriod.text = "per " + suggestedSubscription.period
+                itemView.setOnClickListener{
+                    callBack(suggestedSubscription.name, suggestedSubscription.price.toString().substringBefore('.'), suggestedSubscription.period)
+                    dialog.dismiss()
+                }
+            }
+        }
+
+        // Create new views (invoked by the layout manager)
+        override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+            // Create a new view, which defines the UI of the list item
+            val view = LayoutInflater.from(viewGroup.context)
+                .inflate(R.layout.suggested_subscription_card, viewGroup, false)
+
+            return ViewHolder(view)
+        }
+
+        // Replace the contents of a view (invoked by the layout manager)
+        override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+
+            // Get element from your dataset at this position and replace the
+            // contents of the view with that element
+            val data = dataSet[position]
+            viewHolder.bind(data, callBack, dialog)
+        }
+
+        // Return the size of your dataset (invoked by the layout manager)
+        override fun getItemCount() = dataSet.size
+}
