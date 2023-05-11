a4b1080946c9d5efa87553f706da9a3bd5d4c661
winkydes <keithlam0110@gmail.com>
2023-05-12 00:06:28 +0800

    dashboard fragment done

--- app/src/main/java/com/example/submarker/fragments/DashboardFragment.kt
+++ app/src/main/java/com/example/submarker/fragments/DashboardFragment.kt
@@ -1,18 +1,47 @@
 package com.example.submarker.fragments
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.submarker.R
 import com.example.submarker.databinding.FragmentDashboardBinding
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import kotlin.math.roundToInt
 
 class DashboardFragment : Fragment() {
     val db = Firebase.firestore
     private var _binding: FragmentDashboardBinding? = null
+    private lateinit var pbVideoStreaming: ProgressBar
+    private lateinit var pbMusicStreaming: ProgressBar
+    private lateinit var pbBills: ProgressBar
+    private lateinit var pbHealthcare: ProgressBar
+    private lateinit var pbOthers: ProgressBar
+
+    private lateinit var tvVideo: TextView
+    private lateinit var tvMusic: TextView
+    private lateinit var tvBills: TextView
+    private lateinit var tvHealthcare: TextView
+    private lateinit var tvOthers: TextView
+
+    private var videoMonthPayment: Float = 0f
+    private var musicMonthPayment: Float = 0f
+    private var billsMonthPayment: Float = 0f
+    private var healthcareMonthPayment: Float = 0f
+    private var othersMonthPayment: Float = 0f
+
+    private var videoYearPayment: Float = 0f
+    private var musicYearPayment: Float = 0f
+    private var billsYearPayment: Float = 0f
+    private var healthcareYearPayment: Float = 0f
+    private var othersYearPayment: Float = 0f
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,15 +54,146 @@ class DashboardFragment : Fragment() {
         (activity as AppCompatActivity?)!!.supportActionBar!!.setDisplayHomeAsUpEnabled(false)
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        Log.d("TAG", "onViewCreated")
+
+        pbVideoStreaming = binding.pbVideoStreaming
+        pbMusicStreaming = binding.pbMusicStreaming
+        pbBills = binding.pbBills
+        pbHealthcare = binding.pbHealthcare
+        pbOthers = binding.pbOthers
+
+        tvVideo = binding.tvVideo
+        tvMusic = binding.tvMusic
+        tvBills = binding.tvBills
+        tvHealthcare = binding.tvHealthcare
+        tvOthers = binding.tvOthers
+
+        val btnMonth = binding.btnMonth
+        val btnYear = binding.btnYear
+
+        btnMonth.setOnClickListener {
+            changePeriodType(videoMonthPayment, musicMonthPayment, billsMonthPayment, healthcareMonthPayment, othersMonthPayment)
+        }
+
+        btnYear.setOnClickListener{
+            changePeriodType(videoYearPayment, musicYearPayment, billsYearPayment, healthcareYearPayment, othersYearPayment)
+        }
+
+        val sharedPref =
+            this.activity?.getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE)
+        val userId = sharedPref?.getString("UUID", "") ?:""
+        db.collection("subscriptions").whereEqualTo("userID", userId).get().addOnSuccessListener { documents ->
+            try {
+                for (document in documents) {
+                    if (document.getString("periodType") == "Month") {
+                        when (document.getString("category")) {
+                            "Video Streaming" -> {
+                                val temp = document.getString("price")
+                                    ?.let { Integer.parseInt(it) }
+                                if (temp != null) {
+                                    videoMonthPayment += temp
+                                }
+                            }
+                            "Music Streaming" -> {
+                                val temp = document.getString("price")
+                                    ?.let { Integer.parseInt(it) }
+                                if (temp != null) {
+                                    musicMonthPayment += temp
+                                }
+                            }
+                            "Bills" -> {
+                                val temp = document.getString("price")
+                                    ?.let { Integer.parseInt(it) }
+                                if (temp != null) {
+                                    billsMonthPayment += temp
+                                }
+                            }
+                            "Healthcare" -> {
+                                val temp = document.getString("price")
+                                    ?.let { Integer.parseInt(it) }
+                                if (temp != null) {
+                                    healthcareMonthPayment += temp
+                                }
+                            }
+                            "Others" -> {
+                                val temp = document.getString("price")
+                                    ?.let { Integer.parseInt(it) }
+                                if (temp != null) {
+                                    othersMonthPayment += temp
+                                }
+                            }
+                        }
+                    } else if (document.getString("periodType") == "Year") {
+                        when (document.getString("category")) {
+                            "Video Streaming" -> {
+                                val temp = document.getString("price")
+                                    ?.let { Integer.parseInt(it) }
+                                if (temp != null) {
+                                    videoYearPayment += temp
+                                }
+                            }
+                            "Music Streaming" -> {
+                                val temp = document.getString("price")
+                                    ?.let { Integer.parseInt(it) }
+                                if (temp != null) {
+                                    musicYearPayment += temp
+                                }
+                            }
+                            "Bills" -> {
+                                val temp = document.getString("price")
+                                    ?.let { Integer.parseInt(it) }
+                                if (temp != null) {
+                                    billsYearPayment += temp
+                                }
+                            }
+                            "Healthcare" -> {
+                                val temp = document.getString("price")
+                                    ?.let { Integer.parseInt(it) }
+                                if (temp != null) {
+                                    healthcareYearPayment += temp
+                                }
+                            }
+                            "Others" -> {
+                                val temp = document.getString("price")
+                                    ?.let { Integer.parseInt(it) }
+                                if (temp != null) {
+                                    othersYearPayment += temp
+                                }
+                            }
+                        }
+                    }
+                }
+
+                changePeriodType(videoMonthPayment, musicMonthPayment, billsMonthPayment, healthcareMonthPayment, othersMonthPayment)
+
+            } catch (ex: Exception) {
+                ex.message?.let { Log.e("TAG", it) }
+            }
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun changePeriodType(videoPayment: Float, musicPayment: Float, billsPayment: Float, healthcarePayment: Float, othersPayment: Float) {
+        val totalPayment = videoPayment + musicPayment + billsPayment + healthcarePayment + othersPayment
+        pbVideoStreaming.progress = 100
+        pbMusicStreaming.progress =
+            ((musicPayment + billsPayment + healthcarePayment + othersPayment) / totalPayment * 100).roundToInt()
+        pbBills.progress = ((billsPayment + healthcarePayment + othersPayment) /totalPayment * 100).roundToInt()
+        pbHealthcare.progress = ((healthcarePayment + othersPayment) / totalPayment * 100).roundToInt()
+        pbOthers.progress = ((othersPayment)/ totalPayment * 100).roundToInt()
+
+        tvVideo.text = " --- Video Streaming ${pbVideoStreaming.progress - pbMusicStreaming.progress}% ($${videoPayment})"
+        tvMusic.text = " --- Music Streaming ${pbMusicStreaming.progress - pbBills.progress}% ($${musicPayment})"
+        tvBills.text = " --- Bills ${pbBills.progress - pbHealthcare.progress}% ($${billsPayment})"
+        tvHealthcare.text = " --- Healthcare ${pbHealthcare.progress - pbOthers.progress}% ($${healthcarePayment})"
+        tvOthers.text = " --- Others ${pbOthers.progress}% ($${othersPayment})"
     }
 }
